78c6a8c10ee53ea0b3c3c6e7a8bca0ac34fe3de2
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2022-02-22 12:40:37 -0300

    create project

---
+++ app/src/main/java/dominando/android/orientacao/MainActivity.kt
@@ -0,0 +1,11 @@
+package dominando.android.orientacao
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
